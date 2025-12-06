0d298b73d3d763cf062069b7307a17ddb6b59580
brycesimm <brycesimmerman@gmail.com>
2025-12-06 14:30:51 -0500

    feat: Initial project setup for Aurora (MAUI app) and .gitignore

---
+++ src/Aurora/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Aurora;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
